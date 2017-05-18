31fbbfb0f1aee1e4ff14dc7d1bd095f9d7507ed9
my name <hello@at.com>
2017-05-18 13:27:45 +0300

    proper compiling script

--- compile.sh
+++ compile.sh
@@ -4,6 +4,7 @@ cd "$path"
 git clone git://git.code.sf.net/p/adminer/git
 cd git
 git pull origin master
+rm -f adminer-*.php
 php compile.php
 version = "1"
 echo "$version"
@@ -11,6 +12,7 @@ cd ..;
 cp git/adminer-*.php adminer.php;
 git add adminer.php;
 version=`cat < git/adminer/include/version.inc.php | grep VERSION`
+echo "$version compiled"
 date=`date +'%Y-%m-%d %H:%M'`
 message="$date $version"
 git commit -m "$message"
